d5fdaa44976bf7edfef5b15c2eb2edc1fbb19d0b
Janne Virranmäki <janne.virranmaki@gmail.com>
2018-09-25 13:45:59 +0300

    Simplify bit shift operation

--- main.cpp
+++ main.cpp
@@ -1,50 +1,48 @@
 #include <iostream>
 #include <bitset>
 
-std::bitset<32> m_head_bits("11111111111111111111111111111111");
-std::bitset<32> m_tail_bits("00000000000000000000000000000000");
+std::bitset<32> head_bits("11111111111111111111111111111111");
+std::bitset<32> tail_bits("00000000000000000000000000000000");
 std::bitset<32> init_bits("10000000000000000000000000000000");
 std::bitset<32> tmp_bits("00000000000000000000000000000000");
 std::bitset<32> cur_bits("00000000000000000000000000000000");
 std::bitset<32> prev_bits;
-void addition (size_t iter_size)
+
+void step(size_t iter_size)
 {
     tmp_bits.reset();
-    cur_bits = m_tail_bits;
-    m_head_bits = m_head_bits << 1;
+    cur_bits = tail_bits;
+    // 1111 1111 -> 1111 1110 -> 1111 1100 -> ...
+    head_bits = head_bits << 1;
     for (size_t bit_counter = 0; bit_counter < iter_size; bit_counter += 1) {
         if (bit_counter == 0) {
+            // 1000 0000 0000 0000
             tmp_bits |= init_bits;
         } else if (bit_counter >= 1) {
-            // xor with previous
+            // 1000 0000 0000 0000 -> 0100 0000 0000 0000 -> 0000 0000 0001
             tmp_bits = tmp_bits >> 1; 
         }
-        //std::cout << 't' << tmp_bits << '\n';
-        m_tail_bits ^= tmp_bits;
+        // 1000 0000 0000 0001 XOR 0100 0000 0000 0000 = 1100 0000 0000 0001
+        tail_bits ^= tmp_bits;
+        // 1100 0000 0000 0001 XOR 1000 0000 0000 0001 = 0100 0000 0000 0000
+        tail_bits ^= prev_bits;
+        // 0100 0000 0000 0000 OR 0000 0000 0000 0001 = 0100 0000 0000 0001
+        tail_bits |= cur_bits;
         
-        m_tail_bits ^= prev_bits;
-        m_tail_bits |= cur_bits;
-        
-        
-        std::cout <<  m_head_bits << '\t' << m_tail_bits << '\n';
-        prev_bits = m_tail_bits;
+        std::cout <<  head_bits << '\t' << tail_bits << '\n';
+        prev_bits = tail_bits;
     }
 }
-
+/*
+	|  head    tail   | -> |  head    tail   |
+	
+	1111 1111 0000 0000 -> 0000 0000 1111 1111
+*/
 int main()
 {
-    size_t tail_iter_size = m_tail_bits.to_string().size();
-    for (size_t bit_counter = 0; bit_counter < tail_iter_size; bit_counter += 1) {
-        if (bit_counter == 0) {
-            m_tail_bits |= init_bits;
-            m_head_bits = m_head_bits << 1;
-        } else if (bit_counter >= 1) {
-            m_tail_bits = m_tail_bits >> 1; 
-        }
-        std::cout <<  m_head_bits << '\t' << m_tail_bits << '\n';
-    }
+    size_t tail_iter_size = tail_bits.to_string().size();
     while (tail_iter_size >= 1) {
+        step(tail_iter_size);
         tail_iter_size = tail_iter_size - 1;
-        addition(tail_iter_size);
     }    
 }
